907dfc6845b1821ce491bf42d5232775a0b34ab6
acampsm <acampsm@cells.es>
2018-05-07 14:01:26 +0000

    Adding Experiments fields and listing from backend

--- src/app/calipso-experiment.ts
+++ src/app/calipso-experiment.ts
@@ -1,5 +1,7 @@
 export class CalipsoExperiment {
     constructor(public id: number,
                 public subject: string,
-                public body: string){ }
+                public body: string,
+                public serial_number:string,
+                public beam_line:string){ }
 }
