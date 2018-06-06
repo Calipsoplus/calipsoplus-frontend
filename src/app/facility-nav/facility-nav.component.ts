42fd8ab3ab70a0e9340d952fc9027cc39109df27
acampsm <acampsm@cells.es>
2018-06-06 14:05:26 +0000

    facility components

--- src/app/facility-nav/facility-nav.component.ts
+++ src/app/facility-nav/facility-nav.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 import { CalipsoplusService } from "../calipsoplus.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-facility-nav',
-  templateUrl: './facility-nav.component.html',
-  styleUrls: ['./facility-nav.component.css']
+  selector: "app-facility-nav",
+  templateUrl: "./facility-nav.component.html",
+  styleUrls: ["./facility-nav.component.css"]
 })
 export class FacilityNavComponent implements OnInit {
+  constructor(
+    public calipsoService: CalipsoplusService,
+    private router: Router
+  ) {}
 
-  constructor(public calipsoService: CalipsoplusService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public getUserName(): string {
+    return this.calipsoService.getLoggedUserName();
+  }
+
+  public isLogged(): boolean {
+    return this.calipsoService.isLogged();
+  }
+
+  public logout() {
+    this.calipsoService.unauth().subscribe(
+      resp => {
+        this.router.navigate(["/"]);
+      },
+      error => {
+        alert("Error in logout");
+      }
+    );
   }
 }
