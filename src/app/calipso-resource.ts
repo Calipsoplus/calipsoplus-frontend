43527eb837f4f80ce0419fe7593c6e85c596f471
acampsm <acampsm@cells.es>
2018-06-07 13:03:33 +0000

    redefinition of container and resource classes

--- src/app/calipso-resource.ts
+++ src/app/calipso-resource.ts
@@ -3,9 +3,8 @@ export class CalipsoResource {
     public experiment: string,
     public machine_name: string,
     public machine_ip: string,
-    public created_date: string,
+    public creation_date: string,
     public last_accessed_date: string,
     public expiration_date: string,
-    public container_info: string,
   ) {}
 }
