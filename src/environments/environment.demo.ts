819a251ec12b96c3c8865e74441ba9ad287722fd
acampsm <acampsm@cells.es>
2018-06-20 14:54:03 +0000

    set production=false

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   backendUrl_calipso: "https://calipsotest.cells.es/calipsoplus-services/",
   guacamoleUrl: "http://calipsotest.cells.es:8080/",
   env : "demo"
