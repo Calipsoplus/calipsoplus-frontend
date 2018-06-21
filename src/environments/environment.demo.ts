17e0104299b866ad9adbac184757537aefbe35e2
acampsm <acampsm@cells.es>
2018-06-21 12:37:39 +0000

    updating url for environments

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   backendUrl_calipso: "https://calipsotest.cells.es/calipsoplus-services/",
-  guacamoleUrl: "http://calipsotest.cells.es:8080/",
+  guacamoleUrl: "https://calipsotest.cells.es/gacces/",
   env : "demo"
 };
