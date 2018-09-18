a02b188289b48b0b90469e1486916ce81ee4426e
acampsm <acampsm@cells.es>
2018-09-18 15:32:42 +0200

    Security control in quotas

--- src/app/select-calipso-quota-form/select-calipso-quota-form.component.ts
+++ src/app/select-calipso-quota-form/select-calipso-quota-form.component.ts
@@ -26,39 +26,51 @@ export class SelectCalipsoQuotaFormComponent implements OnInit {
   ngOnInit() {
     if (this.calipsoService.isLogged()) {
       let username = this.calipsoService.getLoggedUserName();
-      this.calipsoService.getCalipsoQuota(username).subscribe(quotas => {
-        this.quotas = quotas;
+      this.calipsoService.getCalipsoQuota(username).subscribe(
+        quotas => {
+          this.quotas = quotas;
 
-        this.calipsoService
-          .getCalipsoAvailableImageQuota(username)
-          .subscribe(used => {
-            this.used_quota = used;
-            this.available_quota.push(new CalipsoQuota(0, 0, "0", "0"));
+          this.calipsoService
+            .getCalipsoAvailableImageQuota(username)
+            .subscribe(used => {
+              this.used_quota = used;
+              this.available_quota.push(new CalipsoQuota(0, 0, "0", "0"));
 
-            for (let index = 0; index < this.quotas.length; index++) {
-              this.available_quota[index].cpu =
-                this.quotas[index].cpu - this.used_quota[index].cpu;
-              this.available_quota[index].memory =
-                parseInt(this.quotas[index].memory.slice(0, -1)) -
-                parseInt(this.used_quota[index].memory.slice(0, -1)) +
-                "G";
-              this.available_quota[index].max_simultaneous =
-                this.quotas[index].max_simultaneous -
-                this.used_quota[index].max_simultaneous;
-              this.available_quota[index].hdd =
-                parseInt(this.quotas[index].hdd.slice(0, -1)) -
-                parseInt(this.used_quota[index].hdd.slice(0, -1)) +
-                "G";
-            }
-          });
-      });
+              for (let index = 0; index < this.quotas.length; index++) {
+                this.available_quota[index].cpu =
+                  this.quotas[index].cpu - this.used_quota[index].cpu;
+                this.available_quota[index].memory =
+                  parseInt(this.quotas[index].memory.slice(0, -1)) -
+                  parseInt(this.used_quota[index].memory.slice(0, -1)) +
+                  "G";
+                this.available_quota[index].max_simultaneous =
+                  this.quotas[index].max_simultaneous -
+                  this.used_quota[index].max_simultaneous;
+                this.available_quota[index].hdd =
+                  parseInt(this.quotas[index].hdd.slice(0, -1)) -
+                  parseInt(this.used_quota[index].hdd.slice(0, -1)) +
+                  "G";
+              }
+            });
+        },
+        error => {
+          this.router.navigate(["/"]);
+          //console.log("Secutiry error");
+        }
+      );
 
       // get default image user for all containers
       this.calipsoService
         .getImageByPublicName("base_image")
         .subscribe(image_quota => {
           this.image_selected = image_quota;
+        },
+        error => {
+          this.router.navigate(["/"]);
+          //console.log("Secutiry error");
         });
+    } else {
+      this.router.navigate(["/"]);
     }
   }
 }
