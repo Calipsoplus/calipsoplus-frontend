cc9883083f2a8436e653cf18917bfce54e0bce16
acampsm <acampsm@cells.es>
2018-09-18 16:37:45 +0200

    partners login

--- src/app/partners-calipso-page/partners-calipso-page.component.ts
+++ src/app/partners-calipso-page/partners-calipso-page.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { CalipsoFacility } from '../calipso-facility';
-import { CalipsoplusService } from '../calipsoplus.service';
+import { Component, OnInit } from "@angular/core";
+import { CalipsoFacility } from "../calipso-facility";
+import { CalipsoplusService } from "../calipsoplus.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-partners-calipso-page',
-  templateUrl: './partners-calipso-page.component.html',
-  styleUrls: ['./partners-calipso-page.component.css']
+  selector: "app-partners-calipso-page",
+  templateUrl: "./partners-calipso-page.component.html",
+  styleUrls: ["./partners-calipso-page.component.css"]
 })
 export class PartnersCalipsoPageComponent implements OnInit {
-
-  constructor(private calipsoService: CalipsoplusService) { }
-
+  constructor(
+    private calipsoService: CalipsoplusService,
+    private router: Router
+  ) {}
 
   facilities: CalipsoFacility[];
 
   ngOnInit() {
-    this.calipsoService.getCalipsoFacilities().subscribe(facilities=>this.facilities=facilities)
+    this.calipsoService
+      .getCalipsoFacilities()
+      .subscribe(facilities => (this.facilities = facilities));
   }
 
+  public enterFacility() {
+    if (this.calipsoService.isLogged()) {
+      this.router.navigate(["/experiment"]);
+    } else {
+      //console.log("You are not logged");
+    }
+  }
+  public isLogged() {
+    return this.calipsoService.isLogged();
+  }
 }
-
