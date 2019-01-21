2deba78bba63d091985b10105356ed7464a618ac
Daniel Sanchez <dsanchez@cells.es>
2019-01-21 13:14:15 +0000

    Remove environment configs from repo, put placeholder examples

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  frontend_calipso: "https://calipsotest.cells.es/",
-  backendUrl_calipso: "https://calipsotest.cells.es/",
-  backendUrl_basehref: "calipsoplus-services/",
-  guacamoleUrl: "https://calipsotest.cells.es/gacces/",
-  env : "prod"
-};
