a4030934a68b4abffd01b9523f6940418322f6c6
Alexandre Camps Mezquita <acampsm@cells.es>
2019-04-04 15:05:38 +0200

    open url or connect to port

--- src/app/select-calipso-experiment-form/select-calipso-experiment-form.component.ts
+++ src/app/select-calipso-experiment-form/select-calipso-experiment-form.component.ts
@@ -490,7 +490,7 @@ export class SelectCalipsoExperimentFormComponent implements OnInit {
 
   public go_in(session: string) {
     const c = this.containers.find(x => x.calipso_experiment === session);
-    if (c == null) { console.log('error win up'); } else if (c.host_port === '') {
+    if (c == null) { console.log('error win up'); } else if (!c.host_port.startsWith('http')) {
       this.calipsoService.go_into_resource(
         c.container_name,
         c.guacamole_username,
