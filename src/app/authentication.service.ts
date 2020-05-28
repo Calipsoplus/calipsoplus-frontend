Calipsoplus/calipsoplus-frontend

commit 37d096a70f9443263fee0b203d2112fd2c52c5d3

Add useroffice URL environment params, remove hardcoded ALBA URL (#117)
* Add useroffice URL, remove hardcoded ALBA URL

As per a discussion with Aidan and Alex, we are
removing the hardcoded UOWebUrl (previously set
to 'https://useroffice.cells.es/Welcome') and
allowing this to be configurable via the frontend
environment.ts files.

The README will also need to be updated.

Currently, the 'enabled' boolean doesn't do anything.

* Add new useroffice stanza to remaining env files

src/app/authentication.service.ts
@@ -17,9 +17,8 @@ export class AuthenticationService {
   uoUserFromHashUrl = this.backendUrl_calipso + 'umbrella/wuo/';
   umbrellaLogoutUrl = this.backendUrl_calipso + 'umbrella/logout/';
   logoutUrl = this.backendUrl_calipso + 'logout/';
-  userUrl = this.backendUrl_calipso + 'users/$USERNAME/';
-  UOWebUrl = 'https://useroffice.cells.es/Welcome';
-
+  userUrl = this.backendUrl_calipso + 'user/$USERNAME/';
+  UOWebUrl = environment.auth.useroffice.url;
 
   constructor(private http: HttpClient, private router: Router) { }
 

src/environments/environment.docker.ts.example
@@ -17,11 +17,15 @@ export const environment = {
     oidc: {
       enabled: false,
       url: 'http://example.com/oidc/authenticate'
+    },
+    useroffice: {
+      enabled: true,
+      url: 'https://USEROFFICE.URL'
     }
   },
   frontend: {
     url: 'http://web-front/',
     facilityLogo: 'assets/images/insert-logo-here.jpg'
   },
-  env : 'docker'
+  env: 'docker'
 };

src/environments/environment.prod.ts.example
@@ -17,7 +17,11 @@ export const environment = {
     oidc: {
       enabled: false,
       url: 'http://example.com/oidc/authenticate'
-    }
+    },
+    useroffice: {
+      enabled: true,
+      url: 'https://USEROFFICE.URL'
+   }
   },
   frontend: {
     url: 'https://calipsofrontend.domain.tld/',

src/environments/environment.ts.example
@@ -22,7 +22,11 @@ export const environment = {
     oidc: {
       enabled: false,
       url: 'http://example.com/oidc/authenticate'
-    }
+    },
+	  useroffice: {
+      enabled: true,
+      url: 'https://USEROFFICE.URL'
+   }
   },
   frontend: {
     url: 'http://calipso-frontend/',

commit 94b4cd898fda9f7d18212c5dbfd16c0f81929d79

Fix user URL naming issue
Commit 37d096a70f944 introduced a regression
where "users" was renamed to "user". This
inadvertently clobbered a previous commit
from Aidan (f2e9164e979a4)

src/app/authentication.service.ts
@@ -17,7 +17,7 @@ export class AuthenticationService {
   uoUserFromHashUrl = this.backendUrl_calipso + 'umbrella/wuo/';
   umbrellaLogoutUrl = this.backendUrl_calipso + 'umbrella/logout/';
   logoutUrl = this.backendUrl_calipso + 'logout/';
-  userUrl = this.backendUrl_calipso + 'user/$USERNAME/';
+  userUrl = this.backendUrl_calipso + 'users/$USERNAME/';
   UOWebUrl = environment.auth.useroffice.url;
 
   constructor(private http: HttpClient, private router: Router) { }